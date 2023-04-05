634d60d3194e95a3ddf74e9b0a08f636ab090685
Sergey <sergey.cherenkov96@gmail.com>
2023-04-05 15:00:30 +0300

    fix text home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
                 <HomeTitle>Serhii Cherenkov</HomeTitle>
                 <p>
                     <span style={{ display: 'block' }}>Greetings all on my page! 👋</span>
-                    I am a junior Front-end Developer and I passionate about creating beautiful user-friendly apps 🤓<br/>
-                    Here you can find a little bit information about my skills and see my portfolio 😊
+                    I am a junior Front-end Developer and I am passionate about creating beautiful user-friendly apps 🤓<br/>
+                    Here you can find a little bit of information about my skills and see my portfolio 😊
                 </p>
             </HomeInfo>
         </HomeWrapper>
