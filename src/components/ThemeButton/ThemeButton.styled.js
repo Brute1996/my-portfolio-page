46907085e2e964489cecbfa5c29fc134d4a4d49f
Sergey <sergey.cherenkov96@gmail.com>
2023-04-14 23:31:33 +0300

    fix theme btn

--- src/components/ThemeButton/ThemeButton.styled.js
+++ src/components/ThemeButton/ThemeButton.styled.js
@@ -2,9 +2,9 @@ import styled from "@emotion/styled";
 
 export const ThemeButtonStyle = styled.button`
   display: flex;
+  position: fixed;
   justify-content: center;
   align-items: center;
-  position: absolute;
   z-index: 9999;
   background-color: rgba(0, 0, 0, 0.4);
   color: #c9c604;
@@ -16,9 +16,10 @@ export const ThemeButtonStyle = styled.button`
 
   transition: scale 300ms ease-in-out;
   top: 2%;
-  left: 200px;
+  left: 208px;
 
   @media (min-width: 768px) {
+    position: absolute;
     top: 2%;
     left: 255px;
   }
